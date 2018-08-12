dfb1d22c22ad3841f00ca9d6debd661414fcb591
Peter <pagrillet@gmail.com>
2018-08-12 11:05:21 +0100

    Its now possible to add items to the form and create an order. Lots to learn but this has been a great overview

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,60 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. Take a copy of the exsiting state
+    const fishes = {...this.state.fishes};
+
+    // 2. Add new fish to fishes
+    fishes[`fish${Date.now()}`] = fish;
+
+    // 3.Update the state
+    this.setState({
+      fishes
+    });
+  }
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes })
+  };
+
+  addToOrder = key => {
+    // 1. Take a copy of state
+    const order = { ...this.state.order };
+    // 2. Add to order or update a number in our order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update the order
+    this.setState({order});
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />))
+            }
+          </ul>
         </div>
+
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     )
   }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-  render(){
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // Stpping form from submitting
+    event.preventDefault();
+    // 2. Get the text from the input
+    const storeName = this.myInput.current.value;
+    // 3. Change the name of the page
+    this.props.history.push(`/store/${storeName}`);
+
+  };
+  render() {
     return (
-      <form className="store-selector" >
+      <form className="store-selector" onSubmit={ this.goToStore }>
         <h2> Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={ this.myInput }
+          required
+          placeholder="Store Name"
+          defaultValue={ getFunName() }
+        />
         <button type="submit">Visit Store</button>
       </form>
-    )
+    );
   }
 }
 
